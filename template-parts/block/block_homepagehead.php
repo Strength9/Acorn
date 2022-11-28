bf68528cacefa88a00714bc918c6a2b3ced0da5b
Strength9 <Strength9@users.noreply.github.com>
2022-11-28 10:44:46 +0000

    Fixed Video Size and quote size

--- template-parts/block/block_homepagehead.php
+++ template-parts/block/block_homepagehead.php
@@ -70,7 +70,7 @@ if (get_field('single_header_text') === 'yes' ) {
 	
 };
 
-$reviewcode = do_shortcode('[trustindex data-widget-id=ef56d0c10f7c59515c1676c46f0]');
+$reviewcode = '<div class="review">'.do_shortcode('[trustindex data-widget-id=ef56d0c10f7c59515c1676c46f0]').'</div>';
 if (get_field('show_video') === 'yes' ) {
 	$hout = '<div class="herodouble"><div class="highlights">'.$headtext.$reviewcode.' </div><div class="videodisp">'.$videohtml.$contactlinksblock.'</div></div>';
 
